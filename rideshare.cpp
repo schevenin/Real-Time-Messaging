f8cc22696af19313e4984ac1111a3ca97e001457
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-04-12 13:45:05 -0700

    Hello, world!

---
+++ rideshare.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file rideshare.cpp
+ * @author Rogelio Schevenin, Sawyer Thompson
+ * @redID 824107681, 823687079
+ * @brief Real-time Messaging for Ridesharing entry point
+ * @date 2022-03-21
+ */
+
+#include "rideshare.h"
+
+/**
+ * @brief Main Execution of Real-time Messaging for Ridesharing
+ *
+ * @param argc integer amount of arguments
+ * @param argv character array of arguments
+ * @return int value indicating exit status
+ */
+int main(int argc, char **argv)
+{
+    printf("Hello, world!\n");
+}
---
+++ rideshare.h
@@ -0,0 +1,7 @@
+#ifndef RIDESHARE_H
+#define RIDESHARE_H
+
+#include <iostream>
+#include <stdio.h>
+
+#endif
